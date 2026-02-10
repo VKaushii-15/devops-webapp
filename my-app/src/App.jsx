5ce6f7e2401b36838fc078ad57a204ef6cec8d66
VKaushii-15 <kaushiik.venkat@gmail.com>
2026-02-10 21:03:22 +0530

    Code Change done in local

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <h2>DevOps code change testing</h2>
+      <h2>DevOps code change testing 2</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
